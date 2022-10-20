f9b9eda0ef99619c383662a772ae06c2649677d0
Kloen Lansfiel <kloen@kloenlansfiel.com>
2022-10-20 16:01:46 +0200

    chore(build): switch from rollup to Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import * as path from "path";
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'demo',
-  resolve:{
-    alias: [
-      { find: '@', replacement: path.resolve('src') }
-    ]
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'Vue3EditorJS',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, './src') }
+    ],
+  }
+});
